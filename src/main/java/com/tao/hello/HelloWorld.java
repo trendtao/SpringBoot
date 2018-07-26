6597e81b3d09ad9256900751cc988058fbc5c739
trendtao <1002978928@qq.com>
2018-07-26 11:04:55 +0800

    1.添加banner.txt 2.添加可以自动部署的代码 3.添加applicaiton配置文件

--- src/main/java/com/tao/hello/HelloWorld.java
+++ src/main/java/com/tao/hello/HelloWorld.java
@@ -1,5 +1,26 @@
 package com.tao.hello;
 
-public class HelloWorld {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@Configuration
+@SpringBootApplication
+public class HelloWorld {
+	
+	@RequestMapping("hello")
+	@ResponseBody
+	public String hello() {
+		return "hello2";
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld.class, args);
+	}
+	
 }
